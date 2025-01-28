f79c014b4246cec449e5fe520b547e760fd531ec
kevindiaz314 <github@kev314.dev>
2025-01-28 18:19:03 -0500

    feat(nvim): configure CodeCompanion with Copilot and Claude as the model

--- dot_config/nvim/lua/plugins/companion.lua
+++ dot_config/nvim/lua/plugins/companion.lua
@@ -4,7 +4,29 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
   },
-  config = true,
+  opts = {
+    display = {
+      chat = {
+        show_settings = true,
+      }
+    },
+    strategies = {
+      chat = {
+        adapter = "copilot",
+      },
+    },
+    adapters = {
+      copilot = function()
+        return require("codecompanion.adapters").extend("copilot", {
+          schema = {
+            model = {
+              default = "claude-3.5-sonnet",
+            },
+          },
+        })
+      end,
+    },
+  },
   keys = {
     { "<leader>cc", ":CodeCompanionChat Toggle<CR>", desc = "Open CodeCompanion" },
     { "<leader>ca", ":CodeCompanionAction<CR>", desc = "Perform CodeCompanion Action" },

bb0fb36e50d910c223f7c1820d9285cec7ba655a
kevindiaz314 <github@kev314.dev>
2025-02-13 23:13:15 -0500

    feat(nvim): add autocmd to trigger chezmoi edit on file enter

--- dot_config/nvim/lua/plugins/chezmoi.lua
+++ dot_config/nvim/lua/plugins/chezmoi.lua
@@ -1,5 +1,14 @@
 return {
   "xvzc/chezmoi.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
-  opts = {}
+  opts = {},
+  init = function()
+    -- run chezmoi edit on file enter
+    vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+      pattern = { os.getenv("HOME") .. "/.local/share/chezmoi/*" },
+      callback = function()
+        vim.schedule(require("chezmoi.commands.__edit").watch)
+      end,
+    })
+  end,
 }

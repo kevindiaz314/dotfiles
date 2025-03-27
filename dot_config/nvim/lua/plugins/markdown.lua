3352089da593a6e4083a10a0cbdd04e33d1348c7
Kevin Diaz <github@kev314.dev>
2025-03-27 16:09:59 -0400

    refactor(nvim): update file type from markdown plugin configuration

--- dot_config/nvim/lua/plugins/markdown.lua
+++ dot_config/nvim/lua/plugins/markdown.lua
@@ -2,7 +2,7 @@ return {
   'MeanderingProgrammer/render-markdown.nvim',
   dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' },
   opts = {
-    file_types = { "markdown", "Avante", },
+    file_types = { "markdown", },
     heading = {
       width = 'block',
       left_pad = 2,

35e88043b350cfe76f1ccfa02624547abb096602
Kevin Diaz <github@kev314.dev>
2025-04-04 17:59:23 -0400

    feat(nvim): remap leader key

--- dot_config/nvim/lua/config/vs-code.lua
+++ dot_config/nvim/lua/config/vs-code.lua
@@ -1,6 +1,10 @@
 local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
+-- Remap leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 -- Clear search highlights
 keymap("n", "<leader>c", ":noh<CR>", opts)
 

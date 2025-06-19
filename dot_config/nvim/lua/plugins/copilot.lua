ab68452edb29d10ba3a8a87b87e542b97f5c94b1
Kevin Diaz <github@kev314.dev>
2025-06-19 12:52:49 -0400

    feat(neovim): update Copilot configuration to enable auto-trigger suggestions

--- dot_config/nvim/lua/plugins/copilot.lua
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -3,35 +3,18 @@ return {
     cmd = "Copilot",
     event = "InsertEnter",
     opts = {
-      suggestion = { enabled = false },
+      suggestion = {
+        auto_trigger = true,
+        keymap = {
+          accept = "<Tab>"
+        }
+      },
       panel = { enabled = false },
       filetypes = {
         markdown = true,
         yaml = true,
       }
-    },
-  },
-  { "saghen/blink.cmp",
-    optional = true,
-    dependencies = {
-      "fang2hou/blink-copilot",
-      opts = {
-        max_completions = 1,
-      }
-    },
-    opts = {
-      sources = {
-        default = { "copilot" },
-        providers = {
-          copilot = {
-            name = "copilot",
-            module = "blink-copilot",
-            score_offset = 100,
-            async = true,
-          },
-        },
-      },
-    },
+    }
   },
   { 'AndreM222/copilot-lualine' },
 }

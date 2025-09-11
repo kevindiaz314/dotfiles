e091eb637f5d5d0da4d54d66839b394f36c754a0
Kevin Diaz <github@kev314.dev>
2025-09-10 23:46:44 -0400

    feat(nvim): add bufferline plugin

---
+++ dot_config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,14 @@
+return {
+  "akinsho/bufferline.nvim",
+  version = "*",
+  event = "VeryLazy",
+  opts = {
+    options = {
+      offsets = {
+        {
+          filetype = "snacks_layout_box",
+        }
+      }
+    }
+  }
+}

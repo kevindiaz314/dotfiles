bfb6681fcc98cc08059c6833c88ae311e4b2a6bb
Kevin Diaz <github@kev314.dev>
2025-09-10 23:51:02 -0400

    refactor(nvim): enhance chezmoi autocommand to handle buffer-specific editing

--- dot_config/nvim/lua/plugins/chezmoi.lua
+++ dot_config/nvim/lua/plugins/chezmoi.lua
@@ -3,11 +3,15 @@ return {
   dependencies = { "nvim-lua/plenary.nvim" },
   opts = {},
   init = function()
-    -- run chezmoi edit on file enter
+    -- Run chezmoi edit on file enter
     vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
       pattern = { os.getenv("HOME") .. "/.local/share/chezmoi/*" },
-      callback = function()
-        vim.schedule(require("chezmoi.commands.__edit").watch)
+      callback = function(ev)
+        local bufnr = ev.buf
+        local edit_watch = function()
+          require("chezmoi.commands.__edit").watch(bufnr)
+        end
+        vim.schedule(edit_watch)
       end,
     })
   end,
